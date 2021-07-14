7d98d239aaf4fb47a5c4a7729177143940772129
CrystalMorris <l.seagypsy.l@gmail.com>
2021-07-14 11:49:57 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const crystalsServer = require('express')
+
+const app = crystalsServer()
+
+const port = 3000
+
+app.use(crystalsServer.static('public'))
+
+app.listen(port, ()=>{
+    console.log(`The server is up and running on http://localhost:${port}`)
+})
