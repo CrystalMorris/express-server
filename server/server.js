14b440169cded8e783842a44a9c76b47b5440491
CrystalMorris <l.seagypsy.l@gmail.com>
2021-07-14 16:02:59 -0500

    added the flipcoin exercise

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
 const crystalsServer = require('express')
+const path = require('path')
 
 const app = crystalsServer()
 
 const port = 3000
 
-app.use(crystalsServer.static('../public'))
+app.use(crystalsServer.static(path.join(__dirname,'public')))
+
+app.get('/flipcoin', async (req, res)=>{
+   
+    let result =  Math.round(Math.random());
+        if(result === 0){
+            result = "heads"
+        } else {
+            result ="tails"
+        }
+    console.log(result)
+    res.json(result)
+    })
+
 
 app.listen(port, ()=>{
     console.log(`The server is up and running on http://localhost:${port}`)
